a34d6aeb54f9659c9566fd2650a11f48c894c665
Alek Angelov <alek.angelov.sk@gmail.com>
2021-04-08 10:43:46 +0200

    adding settings sync with chrome account

--- src/lib/hooks/useWeather.tsx
+++ src/lib/hooks/useWeather.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useAxios from './useAxios';
 import oauthInterceptor from '../interceptors/oauthInterceptor';
 
